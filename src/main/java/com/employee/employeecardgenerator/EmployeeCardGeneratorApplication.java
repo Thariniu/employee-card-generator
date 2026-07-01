b547903ad22ca37ca640b3540f767dce5ceab25c
Tharini Udupi <thariniu93@gmail.com>
2026-07-01 19:01:04 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/employee/employeecardgenerator/EmployeeCardGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.employee.employeecardgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCardGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeCardGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/employee/employeecardgenerator/EmployeeCardGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.employeecardgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCardGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
